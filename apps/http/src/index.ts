51127aff56c50840c8727de0ee1ecaaeaa269976
Mukul Pretham <mukulpretham@gmail.com>
2025-07-27 14:21:09 +0530

    cd

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -39,7 +39,8 @@ app.post("/signup",async(req,res)=>{
 })
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`http Server is running on http://localhost:${port}`);
+  console.log("http")
 });
 
 
